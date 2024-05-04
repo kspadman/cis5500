15945b55d00c71bbbab0c90471ef4c29be959f2d
joshuali99 <joshuali99@gmail.com>
2024-05-04 12:09:31 -0400

    Connected to backend

--- cis5500-project-bball/app/players/[playername]/page.tsx
+++ cis5500-project-bball/app/players/[playername]/page.tsx
@@ -1,19 +1,32 @@
+"use client";
+
 import "./playerpage.css";
 import Image from "next/image";
-
+import {useEffect, useState } from "react";
 
 export default function Page({params} : {params: {playername: string}}) {
-    return <div className = "PlayerPage">
 
+    const [player, setPlayer] = useState(null);
+
+        useEffect(() => {    fetch('http://localhost:3001/api/user', )
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setPlayer(data)})
+            .catch(error => console.error('Error fetching data:', error));
+    }, [])
+      
+    return <div className = "PlayerPage">
         <div className = "PlayerPage-top">
             <div className = "PlayerPage-top-img">
-                <Image src = "/" width = {30} height = {30}>
+                <Image src = "/" width = {30} height = {30} alt = "Photo of player">
 
                 </Image>
             </div>
             <div className = "PlayerPage-top-description">
                 <div className = "PlayerPage-top-name">Jayson Tatum</div>
                 <div className = "PlayerPage-top-info">5 points</div>
+                <div className = "PlayerPage-top-stat-summary">{JSON.stringify(player)}</div>
             </div>
         </div>
         <div className = "PlayerPage-content"></div>
--- nodejs_backend/index.js
+++ nodejs_backend/index.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
+app.use(cors({
+    origin: '*',
+  })); //Change later, this is potentially a security issue
+  
 const PORT = process.env.PORT || 3001;
 
 app.get('/api/user', (req, res) => {
